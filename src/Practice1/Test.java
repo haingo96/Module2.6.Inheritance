8716043d5e73683cb716d868776e370dbf05f0d7
haingo96 <ngohoanghai96@gmail.com>
2023-03-28 10:24:02 +0700

    upload exercise4

---
+++ src/Practice1/Test.java
@@ -0,0 +1,8 @@
+package Practice1;
+
+public class Test {
+    public static void main(String[] args) {
+        Triagle triagle = new Triagle(3, 4, 5);
+        System.out.println(triagle);
+    }
+}
